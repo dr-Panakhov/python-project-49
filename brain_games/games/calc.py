95bbf8e6b36ffc48bb54b371c0646bafbb3206e0
Rafael Panakhov <rafael.panaxov14@gmail.com>
2025-08-12 04:57:45 +0300

    ..

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -7,12 +7,12 @@ def log_gen_ziffer():
     operation = random.choice(['+', '-', '*'])
     number1 = random.randrange(1, 50)
     number2 = random.randrange(1, 50)
-    result = f'{number1}{operation}{number2}'
+    question = f'{number1}{operation}{number2}'
     if operation == '+':
         correct_answer = number1 + number2
     elif operation == '-':
         correct_answer = number1 - number2
     else:
         correct_answer = number1 * number2
-    return result, str(correct_answer)
+    return question, str(correct_answer)
 
