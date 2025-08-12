0bff9f7d7182059495980bce85b457e133cb7b53
Rafael Panakhov <rafael.panaxov14@gmail.com>
2025-08-13 00:07:32 +0300

    fix 2

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,18 +1,18 @@
-from random import randint, randrange, choice
+from random import randint, choice
 
 beschreibung = 'What is the result of the expression?'
 
 
 def log_gen_ziffer():
     operation = choice(['+', '-', '*'])
-    number1 = randrange(1, 50)
-    number2 = randrange(1, 50)
+    number1 = randint(1, 15)
+    number2 = randint(1, 15)
     question = f'{number1}{operation}{number2}'
     if operation == '+':
         correct_answer = number1 + number2
     elif operation == '-':
         correct_answer = number1 - number2
-    else:
+    elif operation == '*':
         correct_answer = number1 * number2
     return question, str(correct_answer)
 
